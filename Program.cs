7b221c8e5154604ab1268c63274419a33a301e2f
Ольга <141999010+olga1234lanina@users.noreply.github.com>
2023-09-12 21:53:28 +0500

    Update Program.cs
    решение

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/ задача 36 . задайте одномерный массив , заполненный случайными числами. найдите сумму элементво , стоящих на нечетных позициях
+// [3,7,23,12]->19      [-4,-6-,89,6]-> 0
+
+Console.Clear();
+int[] array = GetArray();
+PrintArray(array);
+Console.Write($" -> {GetSum(array)}");
+
+int[] GetArray()
+{
+  int size = new Random().Next(5, 10);
+  int[] array = new int[size];
+  for (int i = 0; i < size; i++)
+  {
+    array[i] = new Random().Next(-100, 100);
+  }
+  return array;
+}
+
+void PrintArray(int[] array)
+{
+  Console.Write($"[{String.Join(", ", array)}]");
+}
+
+int GetSum(int[] array)
+{
+  int sum = 0;
+  for (int i = 1; i < array.Length; i++)
+  {
+    if (i % 2 != 0) 
+    {
+      sum += array[i];
+    }
+  }
+  return sum;
+}
